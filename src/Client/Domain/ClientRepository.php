dcd39bcb4974bc016b7b7955436d655f831cfbfb
Aleksei Deviatov <aleksei.deviatov@gmail.com>
2024-10-16 08:35:33 -0400

    add client repository

--- src/Client/Domain/ClientRepository.php
+++ src/Client/Domain/ClientRepository.php
@@ -3,7 +3,13 @@ declare(strict_types=1);
 
 namespace App\Client\Domain;
 
+use App\Common\Domain\ValueObject\Uuid;
+
 interface ClientRepository
 {
+    public function save(Client $client): void;
 
+    public function findByEmail(string $email): ?Client;
+
+    public function findById(Uuid $id): ?Client;
 }
--- src/Client/Infrastructure/Repository/InMemoryClientRepository.php
+++ src/Client/Infrastructure/Repository/InMemoryClientRepository.php
@@ -3,7 +3,33 @@ declare(strict_types=1);
 
 namespace App\Client\Infrastructure\Repository;
 
-class InMemoryClientRepository
-{
+use App\Client\Domain\Client;
+use App\Client\Domain\ClientRepository;
+use App\Common\Domain\ValueObject\Uuid;
 
+class InMemoryClientRepository implements ClientRepository
+{
+    /** @psalm-var array<string, Client> */
+    private array $clients = [];
+
+    public function save(Client $client): void
+    {
+        $this->clients[$client->id()->value] = $client;
+    }
+
+    public function findByEmail(string $email): ?Client
+    {
+        foreach ($this->clients as $client) {
+            if ($client->details()->contacts()->email() === $email) {
+                return $client;
+            }
+        }
+
+        return null;
+    }
+
+    public function findById(Uuid $id): ?Client
+    {
+        return $this->clients[$id->value];
+    }
 }
