a76d87625f822612cfd365c0a4b2f324e9be8f4c
Aleksei Deviatov <aleksei.deviatov@gmail.com>
2024-10-19 23:56:24 -0400

    add simple query handler test

---
+++ tests/Functional/Application/Query/FindClientByIdQueryHandlerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Application\Query;
+
+use App\Clients\Application\Query\FindClientByIdQuery;
+use App\Clients\Domain\Model\Client;
+use App\Shared\Domain\Bus\Query\QueryBus;
+use App\Shared\Domain\ValueObject\Uuid;
+use DataFixtures\ClientFixture;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class FindClientByIdQueryHandlerTest extends WebTestCase
+{
+    private QueryBus $bus;
+
+    /** @var AbstractDatabaseTool */
+    protected AbstractDatabaseTool $databaseTool;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->bus = static::getContainer()->get(QueryBus::class);
+        $this->databaseTool = static::getContainer()->get(DatabaseToolCollection::class)->get();
+    }
+
+    public function testUserExists(): void
+    {
+        $this->databaseTool->loadFixtures([
+            ClientFixture::class,
+        ]);
+
+        $id = new Uuid(ClientFixture::CLIENTS[0]['id']);
+
+        $result = $this->bus->execute(new FindClientByIdQuery($id));
+
+        self::assertInstanceOf(Client::class, $result);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        unset($this->databaseTool);
+    }
+}
