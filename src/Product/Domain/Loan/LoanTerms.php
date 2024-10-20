13f30a0cf104e05c4c68c1671f81e5e00a42d424
Aleksei Deviatov <aleksei.deviatov@gmail.com>
2024-10-20 17:06:58 -0400

    fix loan mapping

--- src/Product/Domain/Loan/LoanTerms.php
+++ src/Product/Domain/Loan/LoanTerms.php
@@ -9,7 +9,7 @@ final readonly class LoanTerms
 {
     public function __construct(
         public int $months,
-        public float $interestRate,
+        public string $interestRate,
         public int $amount,
     )
     {
@@ -19,7 +19,8 @@ final readonly class LoanTerms
     public function validate(): void
     {
         Assert::greaterThan($this->months, 0, 'Loan months must be greater than 0');
-        Assert::greaterThan($this->interestRate, 0, 'Interest rate must be greater than 0');
+        Assert::numeric($this->interestRate, 'Interest rate must be a number');
+        Assert::greaterThan((float)$this->interestRate, 0, 'Interest rate must be greater than 0');
         Assert::greaterThan($this->amount, 0, 'Amount must be greater than 0');
     }
 }
