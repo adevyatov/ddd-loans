7b02f2e29c55b759372ffabb3c9820f6bf1f4aa7
Aleksei Deviatov <aleksei.deviatov@gmail.com>
2024-10-20 17:11:42 -0400

    generate products migration

---
+++ src/Shared/Infrastructure/Doctrine/Migrations/Version20241020211107.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241020211107 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE products_loan (id UUID NOT NULL, months INT NOT NULL, interest_rate NUMERIC(4, 2) NOT NULL, amount INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN products_loan.id IS \'(DC2Type:uuid)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE products_loan');
+    }
+}
