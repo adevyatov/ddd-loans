a08cbfc75ee7c574767074ff74872672e2be7465
Aleksei Deviatov <aleksei.deviatov@gmail.com>
2024-10-25 23:11:46 -0400

    add symfony validator

--- src/Products/Presentation/Controller/CheckLoanEligibilityRequest.php
+++ src/Products/Presentation/Controller/CheckLoanEligibilityRequest.php
@@ -4,18 +4,16 @@ declare(strict_types=1);
 
 namespace App\Products\Presentation\Controller;
 
-use Webmozart\Assert\Assert;
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\Uuid;
 
 final readonly class CheckLoanEligibilityRequest
 {
-    public function __construct(public string $clientId, public int $incomePerMonth)
-    {
-        $this->validate();
-    }
-
-    private function validate(): void
-    {
-        Assert::uuid($this->clientId, 'Client ID should be a valid UUID.');
-        Assert::greaterThan($this->incomePerMonth, 0, 'Income per month is required.');
+    public function __construct(
+        #[Uuid]
+        public string $clientId,
+        #[GreaterThan(0)]
+        public int $incomePerMonth
+    ) {
     }
 }
